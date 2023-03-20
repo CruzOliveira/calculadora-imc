7ad8ca67d1d2ee2a616bbb8fbe7e44f916add90a
Matheus Cruz Oliveira <mccruz2611@gmail.com>
2023-03-20 02:31:51 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './routes';
 import './index.css';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <Provider>
+      <Router />
+    </Provider>
   </React.StrictMode>,
 );
